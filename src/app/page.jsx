37737cff3b4864b4b92641bbe7711bee7839d989
ulysses-ck <ulisesapaza.280@gmail.com>
2024-04-24 10:42:26 -0300

    feat: no cache

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,22 @@
 import ListPosts from "@/components/ListPosts";
 
+export const revalidate = 10;
+
 export default async function Home() {
   const result = await fetch(`${process.env.STRAPI_BACKEND_URL}/api/posts`, {
     headers: {
       Authorization: `Bearer ${process.env.STRAPI_TOKEN_SECRET}`,
     },
+    cache: "no-cache"
   });
 
-  const data = await result.json()
+  const data = await result.json();
+
+
   return (
     <main className="flex flex-col">
       <h1 className="text-2xl text-center">Blog Next.js Strapi</h1>
-      <ListPosts posts={data.data} />
+      {result && <ListPosts posts={data.data} />}
     </main>
   );
 }
